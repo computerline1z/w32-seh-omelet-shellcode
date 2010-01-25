1d551066b2d24e3ee4fd3cf5c35c9f0fea476a81
berendjanwever <berendjanwever@5d02a82a-1227-11de-aa17-9100d24c02a7>
2010-01-25 10:38:12 +0000

    SkyBuild configuration

---
+++ build_config.py
@@ -0,0 +1,11 @@
+build_config = {
+  "projects": {
+    u'w32-seh-omelet-shellcode': {
+      "files": {
+        u'w32-seh-omelet-shellcode.bin': {
+          "sources": [u'w32-seh-omelet-shellcode.asm']
+        },
+      }
+    }
+  }
+}
